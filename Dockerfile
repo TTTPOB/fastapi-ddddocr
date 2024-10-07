0b85df880f1ea451a0817f532e129fc6ce791bf1
tpob <tpob@tpob.xyz>
2024-10-07 15:26:01 +0800

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# Use the official Python image from the Docker Hub
+FROM python:3.12-slim
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the pyproject.toml and poetry.lock files to the container
+COPY pyproject.toml poetry.lock ./
+
+# Install Poetry
+RUN pip install poetry
+
+# Install the dependencies
+RUN poetry install --no-root
+
+# Copy the rest of the application code to the container
+COPY fastapi_ddddocr/ ./fastapi_ddddocr/
+VOLUME /app/config.yaml
+
+# Expose the port the app runs on
+EXPOSE 8000
+
+# Command to run the application
+CMD ["poetry", "run", "python", "-m", "fastapi_ddddocr", "--config", "app/config.yaml"]
---
+++ fastapi_ddddocr/app.py
@@ -0,0 +1,81 @@
+import argparse
+import base64
+import io
+from typing import Annotated
+
+import ddddocr
+import yaml
+from fastapi import FastAPI, File, Form, HTTPException, Request, UploadFile
+from fastapi.responses import JSONResponse
+from PIL import Image
+
+app = FastAPI()
+ocr = ddddocr.DdddOcr()
+global allowed_auth_tokens
+allowed_auth_tokens = []
+
+
+def read_config(config_path):
+    with open(config_path, "r") as file:
+        config = yaml.safe_load(file)
+    return config
+
+
+def perform_ocr(image, probability, png_fix):
+    return ocr.classification(image, png_fix=png_fix, probability=probability)
+
+
+@app.post("/ocr")
+async def ocr_endpoint(
+    auth_token: Annotated[str, Form()],
+    file: Annotated[UploadFile | None, File()] = None,
+    img_b64: Annotated[str | None, Form()] = None,
+    probability: Annotated[bool, Form()] = False,
+    png_fix: Annotated[bool, Form()] = False,
+):
+    global allowed_auth_tokens
+    if auth_token not in allowed_auth_tokens:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
+    if (file is None and img_b64 is None) or (file is not None and img_b64 is not None):
+        raise HTTPException(
+            status_code=400,
+            detail="Either file or image must be provided, but not both",
+        )
+
+    try:
+        if file:
+            image_data = await file.read()
+            img_b64 = Image.open(io.BytesIO(image_data))
+        else:
+            image_data = base64.b64decode(img_b64)
+            img_b64 = Image.open(io.BytesIO(image_data))
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Invalid image data")
+
+    ocr_result = perform_ocr(img_b64, probability, png_fix)
+    if isinstance(ocr_result, str):
+        resp = JSONResponse(content={"ocr_result": ocr_result})
+    elif isinstance(ocr_result, dict):
+        resp = JSONResponse(content=ocr_result)
+    else:
+        raise HTTPException(status_code=500, detail="Unknown error")
+    return resp
+
+
+def main():
+    global allowed_auth_tokens
+    import uvicorn
+
+    parser = argparse.ArgumentParser(description="FastAPI OCR Service")
+    parser.add_argument(
+        "--config", "-c", type=str, required=True, help="Path to the config file"
+    )
+    args = parser.parse_args()
+
+    config = read_config(args.config)
+    allowed_auth_tokens = config["auth_token"]
+    host = config.get("host", "127.0.0.1")
+    port = config.get("port", 8000)
+
+    uvicorn.run(app, host=host, port=port)
